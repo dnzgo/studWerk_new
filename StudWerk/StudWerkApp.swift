76d01efdea9f5710c673245ba19f5e06300697fb
dnzgo <dnzgzc.98@gmail.com>
2026-01-05 22:54:55 +0100

    project started

---
+++ StudWerk/StudWerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudWerkApp.swift
+//  StudWerk
+//
+//  Created by Deniz Gözcü on 05.01.26.
+//
+
+import SwiftUI
+
+@main
+struct StudWerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
